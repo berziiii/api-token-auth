65c3dde914cbffe46bc5ab090baa40ad0c9f362b
berziiii <berzelba@gmail.com>
2016-09-02 12:57:38 -0400

    setTimeout demo

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -3,6 +3,13 @@
 const authEvents = require('./auth/events.js');
 
 // On document ready
+<<<<<<< Updated upstream
 $(() => {
   authEvents.addHandlers();
 });
+=======
+$(window.setTimeout(() => {
+  const circular = require('./circular/index'); // fuggetaboutit
+  authEvents.addHandlers();
+}, 0));
+>>>>>>> Stashed changes
